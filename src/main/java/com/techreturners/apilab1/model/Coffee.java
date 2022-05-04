6103d2c974bd7f14f6798cfe464489017aa9c3c9
charukiesz <charulatapatel2002@yahoo.co.uk>
2022-05-04 21:31:58 +0100

    feat: add HomeController.java, CoffeeController.java, HomeControllerTests.java, CoffeeControllerTests.java, Coffee.java

---
+++ src/main/java/com/techreturners/apilab1/model/Coffee.java
@@ -0,0 +1,23 @@
+package com.techreturners.apilab1.model;
+
+//POJO
+public class Coffee {
+
+    private final long id;
+    private final String name;
+
+
+    public Coffee(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
